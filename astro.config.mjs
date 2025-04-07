e9b15cdc38f5e9e62fb06ced95a021b038284f2b
sam <2740878+samjay000@users.noreply.github.com>
2025-04-07 11:54:34 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import AutoImport from "astro-auto-import";
+import { defineConfig } from "astro/config";
+import remarkCollapse from "remark-collapse";
+import remarkToc from "remark-toc";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://janedoe.com",
+  base: "/",
+  trailingSlash: "ignore",
+  prefetch: {
+    prefetchAll: true
+  },
+  integrations: [react(), sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), AutoImport({
+    imports: ["@components/common/Button.astro", "@shortcodes/Accordion", "@shortcodes/Notice", "@shortcodes/Youtube", "@shortcodes/Tabs", "@shortcodes/Tab"]
+  }), mdx()],
+  markdown: {
+    remarkPlugins: [remarkToc, [remarkCollapse, {
+      test: "Table of contents"
+    }], remarkMath],
+    rehypePlugins: [[rehypeKatex, {}]],
+    shikiConfig: {
+      theme: "dark-plus", // https://shiki.style/themes
+    },
+    extendDefaultPlugins: true
+  },
+});
